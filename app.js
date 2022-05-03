75735b1ed6d26b958945c85497c9663d48aed297
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-03 14:50:08 -0700

    Tracking each submission

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-
+const form = document.getElementById('add-ingredient');
 // let state
 
 // set event listeners 
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submitting the form');
+  //adding code to track each submission to the ingredients
+    const formData = new FormData(form);
+    console.log(formData.get('ingredient'));
+    console.log(formData.get('quantity'));
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
