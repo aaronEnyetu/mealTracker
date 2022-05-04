6b7d489685d54e670399a6d8eb11a437288072da
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-04 12:07:49 -0700

    Created a render meal function

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-import { renderIngredient } from './utils.js';
+import { renderIngredient, renderMealsList } from './utils.js';
 // import functions and grab DOM elements
 const form = document.getElementById('add-ingredient');
 const ingredientsList = document.getElementById('ingredients-list');
 
 const remove = document.getElementById('remove');
 
+const mealName = document.getElementById('meal-report');
+const mealInput = document.getElementById('meal-input');
+const saveMeals = document.getElementById('save-meal');
+//const mealList = document.getElementById('meal-report');
+
 // let state
 let ingredientsArray = [];
+let mealsArray = [];
 
 
 
@@ -56,6 +62,33 @@ function renderIngredients() {
         ingredientsList.appendChild(li);
     }
 }
+
+saveMeals.addEventListener('click', () => {
+    const mealname = mealInput.value;
+    console.log(mealname);
+    let totalIngredients = ingredientsArray.length;
+    mealsArray.push({ mealname, totalIngredients, });
+   
+    
+    resetIngredientList();
+    renderMealsLists();
+
+});
+
+
+function renderMealsLists() {
+    
+    mealName.textContent = ''; 
+    for (let meal of mealsArray) {
+        const li = renderMealsList(meal);
+        mealName.append(li);
+    }
+}
+function resetIngredientList() {
+    ingredientsList.textContent = '';
+
+}
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- utils.js
+++ utils.js
@@ -3,3 +3,9 @@ export function renderIngredient(item) {
     li.textContent = `${item.ingredient} (${item.quantity} Quantity) (${item.measurement} Measurement)`;
     return li;
 }
+
+export function renderMealsList(meal) {
+    const li = document.createElement('li');
+    li.textContent = `${meal.mealname} - ${meal.totalIngredients} ingredients`;
+    return li;
+}
